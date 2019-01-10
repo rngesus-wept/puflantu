3f7e33c7cbb19683c9f08f4b6a6e9953d121b0f7
Charles Tam <me@charlest.net>
2019-01-10 07:01:18 -0500

    Stuff

--- morphemes.py
+++ morphemes.py
@@ -39,6 +39,8 @@ class Inflectable(object):
     return []
 
 
+# Maintain dictionaries of all the possible inflection parameter values
+# This helps us do iteration and express certain morpheme choices more easily later
 TENSES = odict({None: '', 'PST': 'im', 'FUT': 'et'})
 ASPECTS = odict({None: '', 'IMP': 'av', 'PRF': 'uy'})
 DEGREES = odict({None: '', 'AUG': 'ag', 'DIM': 'yi'})
@@ -46,12 +48,17 @@ INVERSION = odict({True: 'vo', False: ''})
 POLARITIES = odict({None: '', 'NEG': 'ey'})
 NOUN_SUFFIXES = odict({'AGT': 'afe', 'PAT': 'who', 'INS': 'aqo',
                        'LOC': 'ice', 'CAU': 'ede', 'GER': 'a'})
+NOUN_CLASSES = [None, 'W', 'T', 'R']
+NUMBERS = odict({None: '', 'DU': 'w', 'PL': 'wa'})
+COMPARISONS = odict({None: '', 'CMP': "'", 'SUP': "'f"})
+ADVERBIAL = odict({True: 'q', False: ''})
+POSSESSIVE = odict({True: 're', False: ''})
+
 
 class VerbLemma(Inflectable):
   """Manage inflections and derivations from a single verb form."""
 
-
-  def __init__(self, lemma, gloss, template=None):
+  def __init__(self, lemma, gloss, template=None, derivative=False):
     assert lemma[-1] in phones.CONSONANTS, \
         'Verb form {} must end in a consonant.'.format(lemma.upper())
 
@@ -64,6 +71,7 @@ class VerbLemma(Inflectable):
     assert len(self.template.split('*')) == 2, \
         'Template {} must contain exactly one "*".'.format(self.template.upper())
     self.gloss = gloss
+    self.derivative = derivative
 
   def GenerateProclitics(self, tense=None, aspect=None, **kwargs):
     result = [TENSES.get(tense, None), ASPECTS.get(aspect, None)]
@@ -83,51 +91,57 @@ class VerbLemma(Inflectable):
     result = [NOUN_SUFFIXES.get(nounify, None)]
     return [_ for _ in result if _]
 
-  def Inflections(self):
+  def Inflections(self, exclude_clitics=False):
     """Generate all valid inflections."""
-    for tense in TENSES:
-      for aspect in ASPECTS:
-        for degree in DEGREES:
-          for inverted in INVERSION:
-            for subject in IterPronounArgs(case=['NOM']):
-              if subject is None:
-                continue
-              for polarity in POLARITIES:
-                for objekt in IterPronounArgs(case=['OBL']):
+    for inverted in INVERSION:
+      for degree in DEGREES:
+        for polarity in POLARITIES:
+          for subject in IterPronounArgs(case=['NOM']):
+            if subject is None:
+              continue
+            for objekt in IterPronounArgs(case=['OBL']):
+              for tense in ([None] if exclude_clitics else TENSES):
+                for aspect in ([None] if exclude_clitics else ASPECTS):
                   yield self.Inflect(tense=tense, aspect=aspect,
                                      degree=degree, polarity=polarity,
                                      subject=subject, objekt=objekt,
                                      inverted=inverted)
+          if not self.derivative:
+            for nounify in NOUN_SUFFIXES:
+              for noun_inflection in NounLemma(
+                  self.Inflect(inverted=inverted, polarity=polarity,
+                               degree=degree, nounify=nounify)[1],
+                  '+'.join([_ for _ in [self.gloss, 'INV' if inverted else '',
+                                        degree, polarity, 'N', nounify] if _]),
+                  derivative=True).Inflections(exclude_clitics=exclude_clitics):
+                yield noun_inflection
 
   def SampleInflection(self):
+    kwargs = {}
+    kwargs['inverted'] = random.choices([True, False], [.2, .8])[0]
+    kwargs['degree'] = random.choices(list(DEGREES), [.8, .1, .1])[0]
+    kwargs['polarity'] = random.choices(list(POLARITIES), [.75, .25])[0]
     if random.random() < .7:
       # Verb
-      kwargs = {
+      kwargs.extend({
         'tense': random.choices(list(TENSES), [.6, .2, .2])[0],
         'aspect': random.choices(list(ASPECTS), [.6, .2, .2])[0],
-        'degree': random.choices(list(DEGREES), [.8, .1, .1])[0],
-        'inverted': random.choices([True, False], [.2, .8])[0],
-        'polarity': random.choices(list(POLARITIES), [.75, .25])[0],
         'subject': (random.choices(['1', '2', '3', 'REL'], [.3, .3, .3, .1])[0],
                     random.choices(['SG', 'DU', 'PL'], [1, 1, 1])[0], 'NOM')
-        }
+        })
       if random.random() < .5:
         kwargs['objekt'] = (random.choices(['1', '2', '3', 'REL'], [.3, .3, .3, .1])[0],
                             random.choices(['SG', 'DU', 'PL'], [1, 1, 1])[0], 'OBL')
     else:
       # Noun
-      kwargs = {
-        'inverted': random.choices([True, False], [.2, .8])[0],
-        'polarity': random.choices(list(POLARITIES), [.75, .25])[0],
-        'nounify': random.choices(list(NOUN_SUFFIXES), [1, 1, 1, 1, 1, 1])[0]
-        }
+      kwargs['nounify'] = random.choices(list(NOUN_SUFFIXES), [1, 1, 1, 1, 1, 1])[0]
     return (self.Inflect(**kwargs), kwargs)
 
 
 class NounLemma(Inflectable):
   """Manage inflections and derivations from a single noun form."""
 
-  def __init__(self, lemma, gloss, template=None):
+  def __init__(self, lemma, gloss, template=None, derivative=False):
     assert lemma[-1] in phones.VOWELS, \
         'Noun form {} must end in a vowel.'.format(lemma.upper())
     assert lemma[-1] is not 'w', \
@@ -139,8 +153,10 @@ class NounLemma(Inflectable):
     self.template = lemma[:-1] + '*'
     self.final_vowel = lemma[-1]
     self.gloss = gloss
+    self.derivative = derivative
 
   def Inflect(self, klass=None, **kwargs):
+    print(kwargs, klass)
     proclitics, main, enclitics = super().Inflect(**kwargs)
 
     ## Now do noun class things
@@ -160,28 +176,54 @@ class NounLemma(Inflectable):
 
     return proclitics, main, enclitics
 
-  def GenerateInfixes(self, polarity=None):
+  def GenerateInfixes(self, polarity=None, **kwargs):
     result = [POLARITIES.get(polarity, None)]
     return [_ for _ in result if _]
 
-  def GenerateSuffixes(self, number=None, compare=None, adverb=None, **kwargs):
+  def GenerateSuffixes(self, number=None, compare=None, adverbial=False, **kwargs):
     result = []
     if compare:
       result.append(self.final_vowel)
-      result.append({'CMP': "'", 'SUP': "'f"}.get(compare))
+      result.append(COMPARISONS.get(compare))
     if number is None or number == 'SG':
       result.append('i' if compare == 'SUP' else self.final_vowel)
     else:
-      result.append({'DU': 'w', 'PL': 'wa'}[number])
-    if adverb:
-      result.append('q')
-    return result
+      result.append(NUMBERS.get(number))
+    result.append(ADVERBIAL.get(adverbial, None))
+    return [_ for _ in result if _]
 
   def GenerateEnclitics(self, possessive=None, **kwargs):
-    result = []
-    if possessive:
-      result.append('re')
-    return result
+    result = [POSSESSIVE.get(possessive, None)]
+    return [_ for _ in result if _]
+
+  def Inflections(self, exclude_clitics=False):
+    """Generate all valid inflections."""
+    for polarity in POLARITIES:
+      for klass in NOUN_CLASSES:
+        for number in NUMBERS:
+          for possessive in ([] if exclude_clitics else POSSESSIVE):
+            yield self.Inflect(polarity=polarity, klass=klass,
+                               number=number, possessive=possessive)
+          for compare in COMPARISONS:
+            yield self.Inflect(polarity=polarity, klass=klass,
+                               number=number, compare=compare)
+      for compare in COMPARISONS:
+        ## Adverbial
+        yield self.Inflect(polarity=polarity, compare=compare, adverbial=True)
+
+  def SampleInflection(self):
+    kwargs = {}
+    kwargs['polarity'] = random.choices(list(POLARITIES), [.75, .25])[0]
+    if random.random() < .1:
+      kwargs['adverbial'] = True
+      kwargs['compare'] = random.choices(list(COMPARISONS), [6, 1, 1])[0]
+    else:
+      kwargs['klass'] = random.choices(list(NOUN_CLASSES), [7, 1, 1, 1])[0]
+      kwargs['number'] = random.choices(list(NUMBERS), [2, 1, 1])[0]
+      kwargs['compare'] = random.choices(list(COMPARISONS), [6, 1, 1])[0]
+      if kwargs['compare'] is None and random.random() < .1:
+        kwargs['possessive'] = True
+    return (self.Inflect(**kwargs), kwargs)
 
 
 def GetPronoun(person,  # 1, 2, 3, or REL
@@ -207,9 +249,6 @@ def IterPronounArgs(person=None, number=None, case=None):
 
 
 if __name__ == '__main__':
-  for _ in range(10):
-    print(VerbLemma('riq', 'drink').SampleInflection())
-  # for proclitic, main, enclitic in VerbLemma('riq', 'drink').Inflections():
-  #   print('{}{}{}'.format((proclitic + ' ') if proclitic else '',
-  #                         main,
-  #                         ('-' + enclitic) if enclitic else ''))
+  ## For debugging purposes
+  for proclitic, main, enclitic in NounLemma('riqwho', 'drinkable').Inflections(exclude_clitics=True):
+    print(proclitic, main, enclitic)
