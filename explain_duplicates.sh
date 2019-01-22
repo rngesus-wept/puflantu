9b402d3cbd5230ad6574bdf337965b973bf03c5e
Charles Tam <me@charlest.net>
2019-01-22 18:46:20 -0500

    Documentation

--- explain_duplicates.sh
+++ explain_duplicates.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Explains the provenance of all duplicates. test_lexicon.py uses a bloom filter
+# for efficient duplicate detection and therefore only lists *probable*
+# duplicates. This script lists the derivation of all duplicates explicitly. As
+# long as each putative duplicate only have one derivation, everything is okay
+# and those false positives can be added to collisions.tsv.
+
 mv collisions.tsv collisions.tsv.tmp;
 
 for DUPLICATE in `python test_lexicon.py 2>&1 | head -n-1 | cut -d' ' -f1`; do
