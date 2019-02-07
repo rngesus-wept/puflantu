393cfd91cd3ab58c90b4321f653ba96f1f6f27e2
Charles Tam <me@charlest.net>
2019-02-07 01:00:52 -0500

    Replace plural -wa with -we
    Updated docs in corresponding places
    Moved possessive r*e/-re to r*o/-re to avoid additional collisions

--- morphemes.py
+++ morphemes.py
@@ -52,7 +52,7 @@ NOUN_POLARITIES = odict({None: '', 'NEG': 'uy'})
 NOUN_SUFFIXES = odict({'AGT': 'afe', 'PAT': 'who', 'INS': 'aqo',
                        'LOC': 'ice', 'CAU': 'ede', 'GER': 'a'})
 NOUN_CLASSES = [None, 'W', 'T', 'R']
-NUMBERS = odict({None: '', 'DU': 'w', 'PL': 'wa'})
+NUMBERS = odict({None: '', 'DU': 'w', 'PL': 'we'})
 COMPARISONS = odict({None: '', 'CMP': "'", 'SUP': "'f"})
 ADVERBIAL = odict({True: 'c', False: ''})
 POSSESSIVE = odict({True: 're', False: ''})
